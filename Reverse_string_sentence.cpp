ef8a7b172e1a6d3639e574fab64f9a688ed65aa0
Pranab Porel <maynaporel529@gmail.com>
2026-04-19 15:46:36 +0530

    Added New Leetcode Problems

---
+++ Reverse_string_sentence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        string result="";
+        while(ss>>word){
+            if (result== "")
+                result = word;
+            else{
+                result=word+" "+result;
+            } 
+        }
+        return result;
+        
+    }
+};
