4f2f6b6b20c5b2734c8d9d6bc964b8670dee18ee
Pranab Porel <maynaporel529@gmail.com>
2026-06-20 23:32:26 +0530

    Added New Leetcode Problems

---
+++ Max_Consecutive_one.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int maxCount= 0;
+
+        for(int num:nums){
+            if(num==1){
+                count++;
+                if(count>maxCount){
+                    maxCount=count;
+                }
+            }
+            else{
+                count=0;
+            }
+        }
+        return maxCount;
+        
+    }
+};
