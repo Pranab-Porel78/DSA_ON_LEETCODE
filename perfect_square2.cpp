3fecbd688e0213cb83ed2daac41f31dac81154b2
Pranab Porel <maynaporel529@gmail.com>
2026-07-05 15:27:22 +0530

    Leetcode Problems

---
+++ perfect_square2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    int solve(int n) {
+        if (n == 0)
+            return 0;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        int ans = INT_MAX;
+
+        for (int i = 1; i * i <= n; i++) {
+            ans = min(ans, 1 + solve(n - i * i));
+        }
+
+        return dp[n] = ans;
+    }
+
+    int numSquares(int n) {
+        dp.resize(n + 1, -1);
+        return solve(n);
+    }
+};
