28e0e4fc070ebaf5c0136bc26bc4800d9d84264f
Pranab Porel <maynaporel529@gmail.com>
2026-07-14 19:16:35 +0530

    Leetcode Problems

---
+++ best_time_buysell2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int n =prices.size();
+        for(int i=0;i<n-1;i++){
+            if(prices[i+1]>prices[i]){
+              int temp  = prices[i+1]-prices[i];
+              profit += temp;
+            }
+        }
+        return profit;
+        
+    }
+};
